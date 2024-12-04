9c2c967c7fd6383821f3b3d3a7e5324420925cfd
Gergely-H <gergely.huszar@proton.me>
2024-12-04 18:37:35 +0100

    feat: Navbar - add component

---
+++ src/components/ui/organisms/navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import { NavButton } from '@/components/ui/molecules/nav-button/NavButton';
+import { getDictionary } from '@/lang/dictionaries';
+
+const dictionary = getDictionary();
+
+type NavbarProps = {
+  isOpen: boolean;
+  onOutsideClick: () => void;
+};
+
+export const Navbar = ({ isOpen, onOutsideClick }: NavbarProps) => {
+  const navbarEntries = Object.values(dictionary.navbar);
+
+  return (
+    <>
+      {isOpen && (
+        <div
+          className='absolute left-0 top-full h-screen w-screen bg-gray-500 opacity-50'
+          onClick={onOutsideClick}
+        />
+      )}
+      <nav
+        className={`
+        absolute top-full h-screen w-1/2 bg-white [transition:right_300ms]
+        md:w-1/3
+        lg:w-1/4
+        ${isOpen ? 'right-0' : '-right-1/2 md:-right-1/3 lg:-right-1/4'}
+      `}
+      >
+        {navbarEntries.map(({ path, label }) => (
+          <NavButton key={path} label={label} path={path} />
+        ))}
+      </nav>
+    </>
+  );
+};
