b25039a8fabb56b205d7c131a93d9ef52afc8064
Gergely-H <gergely.huszar@proton.me>
2024-12-07 13:30:11 +0100

    feat: prisma - add schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-const HELLO_WORLD = 'HELLO WORLD';
+import { prisma } from '@/database/prisma';
 
-const Home = () => (
-  <div>
-    <span>{HELLO_WORLD}</span>
-  </div>
-);
+const Home = async () => {
+  const user = await prisma.user.findFirst({
+    where: { name: 'FirstTestUser' },
+  });
+
+  return (
+    <div>
+      <span>{user?.id}</span>
+      <span>{user?.name}</span>
+      <span>{user?.email}</span>
+      <span>{user?.createdAt.toDateString()}</span>
+    </div>
+  );
+};
 
 export default Home;
