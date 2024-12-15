b464cc9b9d9a65080061ef2ebdb2cb1e29d71e6a
Gergely-H <gergely.huszar@proton.me>
2024-12-15 17:30:38 +0100

    refact: Navbar - update identation

--- src/components/ui/organisms/navbar/Navbar.tsx
+++ src/components/ui/organisms/navbar/Navbar.tsx
@@ -21,11 +21,11 @@ export const Navbar = ({ isOpen, onOutsideClick }: NavbarProps) => {
       )}
       <nav
         className={`
-        absolute top-full h-screen w-1/2 bg-white [transition:right_300ms]
-        md:w-1/3
-        lg:w-1/4
-        ${isOpen ? 'right-0' : '-right-1/2 md:-right-1/3 lg:-right-1/4'}
-      `}
+          absolute top-full h-screen w-1/2 bg-white [transition:right_300ms]
+          md:w-1/3
+          lg:w-1/4
+          ${isOpen ? 'right-0' : '-right-1/2 md:-right-1/3 lg:-right-1/4'}
+        `}
       >
         {navbarEntries.map(({ path, label }) => (
           <NavButton key={path} label={label} path={path} />
