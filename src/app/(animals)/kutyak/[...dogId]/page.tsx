360dfbdba89533a3ca1b107effa780bb9d62292a
Gergely-H <gergely.huszar@proton.me>
2024-12-16 22:32:54 +0100

    feat: DogDetails - add component

---
+++ src/app/(animals)/kutyak/[...dogId]/page.tsx
@@ -0,0 +1,76 @@
+import { Image } from '@/components/ui/atoms/image/Image';
+import { prisma } from '@/prisma/prisma';
+import type { Animal } from '@prisma/client';
+
+const fallbackDog: Animal = {
+  id: 'totallyOriginalId',
+  name: 'Sheriff',
+  age: 10,
+  description: "Hello, I'm Sheriff. I'm a dog.",
+  sex: 'goodboy',
+  imageUrls: [],
+  profilePicUrl: null,
+};
+
+const getDogById = async (id: string) => {
+  const { animal: dog } =
+    (await prisma.dog.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        animal: true,
+      },
+    })) ?? {};
+
+  return dog ?? fallbackDog;
+};
+
+type DogDetailsProps = {
+  params: Promise<{ dogId: string[] }>;
+};
+
+const DogDetails = async ({ params }: DogDetailsProps) => {
+  const {
+    dogId: [id],
+  } = await params;
+
+  const dog = await getDogById(id);
+
+  return (
+    <div className='flex flex-col'>
+      <div
+        className={`
+          flex flex-col flex-wrap
+          md:flex-row
+        `}
+      >
+        <div
+          className={`
+            relative h-[450px] w-full
+            md:w-1/2
+          `}
+        >
+          <Image
+            alt='doggo'
+            fill
+            priority
+            src={dog.profilePicUrl ?? '/herosz-budapest-logo.svg'}
+            style={{ objectFit: 'cover' }}
+          />
+        </div>
+        <div>
+          <p>{dog.id}</p>
+          <p>{dog.age}</p>
+          <p>{dog.sex}</p>
+          <p>{dog.imageUrls}</p>
+        </div>
+      </div>
+      <div>
+        <p>{dog.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DogDetails;
