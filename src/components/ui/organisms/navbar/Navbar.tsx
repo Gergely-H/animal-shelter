9f4c6913490ac41fd8834ebf696da996dcfa2101
Gergely-H <gergely.huszar@proton.me>
2024-12-20 13:17:26 +0100

    feat: Navbar - add transition to navbar backdrop

--- src/components/ui/organisms/navbar/Navbar.tsx
+++ src/components/ui/organisms/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { NavButton } from '@/components/ui/molecules/nav-button/NavButton';
 import { getDictionary } from '@/languages/dictionaries';
+import { useEffect, useRef } from 'react';
 
 const dictionary = getDictionary();
 
@@ -9,16 +10,28 @@ type NavbarProps = {
 };
 
 export const Navbar = ({ isOpen, onOutsideClick }: NavbarProps) => {
+  const navbarBackdrop = useRef<HTMLDivElement>(null);
   const navbarEntries = Object.values(dictionary.navbar);
+
+  useEffect(() => {
+    navbarBackdrop.current?.classList.add('!size-0');
+  }, []);
 
   return (
     <>
-      {isOpen && (
-        <div
-          className='absolute left-0 top-full h-screen w-screen bg-gray-500 opacity-50'
-          onClick={onOutsideClick}
-        />
-      )}
+      <div
+        className={`
+            absolute left-0 top-full h-screen w-screen bg-gray-500 [transition:opacity_300ms]
+            ${isOpen ? 'opacity-50' : 'opacity-0'}
+          `}
+        onClick={onOutsideClick}
+        onTransitionEnd={() => {
+          if (!isOpen) {
+            navbarBackdrop.current?.classList.toggle('!size-0');
+          }
+        }}
+        ref={navbarBackdrop}
+      />
       <nav
         className={`
           absolute top-full h-screen w-1/2 bg-white [transition:right_300ms]
