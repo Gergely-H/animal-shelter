5b4da6db5ec237747f974a5ecd81814510c416f6
Gergely-H <gergely.huszar@proton.me>
2024-12-20 15:13:01 +0100

    refact: Image - replace props type with next type

--- src/components/ui/atoms/image/Image.tsx
+++ src/components/ui/atoms/image/Image.tsx
@@ -1,9 +1,5 @@
+import type { ImageProps } from 'next/image';
 // eslint-disable-next-line consistent-default-export-name/default-import-match-filename, no-restricted-imports -- This rule is intended for project code, not for libraries or built-in APIs. This is the only exception where import of NextImage is allowed.
 import NextImage from 'next/image';
-import type { ComponentProps } from 'react';
 
-type ImageProps = ComponentProps<typeof NextImage>;
-
-export const Image = (props: ImageProps) => (
-  <NextImage draggable={false} {...props} />
-);
+export const Image = (props: ImageProps) => <NextImage draggable={false} {...props} />;
